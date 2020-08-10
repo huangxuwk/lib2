a731d13e4e8ca65b512ec97d55d24779d7755b54
huangxuwk <56286527+huangxuwk@users.noreply.github.com>
2020-08-10 16:21:07 +0800

    Add files via upload

---
+++ 20200810@LeetCode@Rotate/Rotate.java
@@ -0,0 +1,25 @@
+package src.com.jd.leetcode.textI;
+
+public class Rotate {
+
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        // 转置
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int tmp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = tmp;
+            }
+        }
+        // 每一行进行翻转
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = tmp;
+            }
+        }
+    }
+
+}
